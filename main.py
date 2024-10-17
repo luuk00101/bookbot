2324c55f9fe6179540dcc45b63602483e7fb437f
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-10-18 00:46:48 +0200

    add character counting

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ def get_word_count(text):
     return len(text.split())
 
 
+def get_character_count(text):
+    text = text.lower()
+
+    character_dict = {}
+
+    for character in text:
+        if character_dict.get(character):
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+
+    return character_dict
+
+
 def main():
     text = load_text()
     print(get_word_count(text))
+    print(get_character_count(text))
 
 
 main()
