c3eeb49ba2eaf470086d8a544e36e04938d5582b
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-10-18 00:41:26 +0200

    refactor the code

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-with open("books/frankenstein.txt") as book:
-    print(len(book.read().split()))
+def load_text():
+    with open("books/frankenstein.txt") as book:
+        return book.read()
+
+
+def get_word_count(text):
+    return len(text.split())
+
+
+def main():
+    text = load_text()
+    print(get_word_count(text))
+
+
+main()
