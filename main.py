0daefdc7cbc79fe9a0e5607890c7a02dee429e50
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-10-18 08:50:51 +0200

    add report generation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def load_text():
-    with open("books/frankenstein.txt") as book:
+def load_text(book_path):
+    with open(book_path) as book:
         return book.read()
 
 
@@ -13,18 +13,43 @@ def get_character_count(text):
     character_dict = {}
 
     for character in text:
-        if character_dict.get(character):
+        if not character.isalpha():
+            continue
+        elif character_dict.get(character):
             character_dict[character] += 1
         else:
             character_dict[character] = 1
 
     return character_dict
 
+def dict_to_sorted_list(dictionary):
+    list = []
+
+    for key in dictionary:
+        list.append([key, dictionary[key]])
+ 
+    list.sort(reverse=True, key=lambda x:x[1])
+
+    return list
+
+def print_report(book_path, text):
+    print(f"--- Begin report of {book_path} ---")
+
+    word_count = get_word_count(text)
+    print(f"{word_count} words found in the document\n")
+
+    character_dict = get_character_count(text)
+    sorted_list = dict_to_sorted_list(character_dict)
+    for value in sorted_list:
+        print(f"The '{value[0]}' character was found {value[1]} times")
+
+    print("--- End report ---")
+
 
 def main():
-    text = load_text()
-    print(get_word_count(text))
-    print(get_character_count(text))
+    book_path = "books/frankenstein.txt"
+    text = load_text(book_path)
+    print_report(book_path, text)
 
 
 main()
